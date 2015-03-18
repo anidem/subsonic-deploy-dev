abf72a8b48b0b6080e186d24e76b90d16e12ca50
r@m <richmedina@gmail.com>
2015-03-18 00:42:09 -1000

    simple change

--- hello/views.py
+++ hello/views.py
@@ -5,7 +5,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Aloha from Python!')
+    return HttpResponse('aHola from Python!')
 
 
 def db(request):
